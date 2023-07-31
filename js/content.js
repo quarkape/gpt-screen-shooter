9a6efb9063a8177ff9153056855f4a82cd45c3d4
quarkape <quarkape@qq.com>
2023-07-31 17:57:09 +0800

    更新至3.0.0版本

--- js/content.js
+++ js/content.js
@@ -10,11 +10,16 @@ const colorB = "#000000"; // 输出内容区域的字色，十六进制表示，
 const ContentWidth = 100; // 整体内容占页面的宽度，如果设置为100，建议字号尽量大
 // const showAvatar = true; // 是否显示头像
 const stickyHeight = 200; // 顶部留空，以防止手机刘海遮住
+const inputPre = "【输入内容】"; //输入内容前缀
+const inputSuf = "【输出内容】"; //输出内容前缀
+const divider = "------"; // 分隔符
+const fileName = "gpt-screen-shooter导出内容"; // 导出文本名称
 
 
 chrome.runtime.onMessage.addListener((req, sender, resp) => {
-  const opt = req.opt;
-  if (opt === 0) {
+  const opt = req;
+  // return;
+  if (opt.type === 0) {
     // 隐藏侧边栏
     const nav = document.querySelector(".bg-gray-900");
     nav.style.display = "none";
@@ -37,90 +42,68 @@ chrome.runtime.onMessage.addListener((req, sender, resp) => {
     whole3.children[1].classList.remove("overflow-hidden");
 
     // 修改顶部内容，增加宽度，删除模型名称
-    const sticky = document.getElementsByClassName("sticky")[2];
+    const sticky = document.getElementsByClassName("sticky")[document.getElementsByClassName("sticky").length - 1];
     sticky.style.height = stickyHeight + "px";
     sticky.innerHTML = "";
 
     // 回复内容样式
-    let gc = document.getElementsByClassName("prose");
+    const gc = document.getElementsByClassName("prose");
     for (let i=0;i<gc.length;i++) {
       gc[i].style.fontWeight = "bold";
-      gc[i].style.fontSize = fontSizeA + "px";
-      gc[i].style.color = colorB;
+      gc[i].style.fontSize = (opt.fontSize ? opt.fontSize : fontSizeB) + "px";
+      gc[i].style.color = opt.outputColor ? `#${opt.outputColor}` : colorB;
     }
 
     // 发送内容样式
-    let sc = document.getElementsByClassName("min-h-[20px]");
+    const sc = document.getElementsByClassName("min-h-[20px]");
     for (let i=0;i<sc.length;i++) {
       sc[i].style.fontWeight = "bold";
-      sc[i].style.lineHeight = (fontSizeB + 8) + "px";
-      sc[i].style.fontSize = fontSizeB + "px";
-      sc[i].style.color = "#000000";
+      sc[i].style.lineHeight = ((Number(opt.fontSize ? opt.fontSize : fontSizeA)) + 16) + "px";
+      sc[i].style.fontSize = (opt.fontSize ? opt.fontSize : fontSizeA) + "px";
+      sc[i].style.color = opt.inputColor ? `#${opt.inputColor}` : colorA;
+    }
+
+    // 隐藏头像
+    if (!opt.showAvatar) {
+      const imgs = document.getElementsByTagName("img");
+      for (const item of imgs) {
+        item.style.display = "none";
+      }
+      const svgs = document.getElementsByClassName("w-[30px]");
+      for (const item of svgs) {
+        item.style.display = "none";
+      }
     }
 
     // 所有内容样式
     const ac = document.getElementsByClassName("lg:max-w-[38rem]");
     for (const item of ac) {
-      item.style.width = ContentWidth + "% !important";
-      item.style.maxWidth = ContentWidth + "%";
-      item.style.left = (100 - ContentWidth)/2 + "%"
+      item.style.width = (opt.width ? Number(opt.width) : ContentWidth) + "% !important";
+      item.style.maxWidth = (opt.width ? Number(opt.width) : ContentWidth) + "%";
+      item.style.left = (100 - (opt.width ? Number(opt.width) : ContentWidth))/2 + "%"
     }
 
     resp("处理成功，请打开浏览器开发者工具，手动截屏");
-  } 
-
-  // if(opt === 1) {
-  //   // 显示侧边栏
-  //   const nav = document.querySelector(".bg-gray-900");
-  //   nav.style.display = "block";
-
-  //   // 显示下方输入框
-  //   const input = document.getElementsByClassName("pt-2")[1];
-  //   input.style.display = "block";
-
-  //   // 恢复
-  //   const whole = document.querySelector(".w-full");
-  //   whole.classList.add("overflow-hidden");
-
-  //   const whole2 = document.querySelector(".max-w-full");
-  //   whole2.classList.add("overflow-hidden");
-
-  //   const whole3 = document.querySelector(".transition-width");
-  //   whole3.classList.add("overflow-hidden");
-
-  //   whole3.children[1].classList.add("overflow-hidden");
-
-  //   // 将回复内容设置样式
-  //   let gc = document.getElementsByClassName("prose");
-  //   for (let i=0;i<gc.length;i++) {
-  //     gc[i].style.fontWeight = "normal";
-  //     gc[i].style.fontSize = "16px";
-  //   }
-
-  //   // 将发送内容设置样式
-  //   let sc = document.getElementsByClassName("min-h-[20px]");
-  //   for (let i=0;i<sc.length;i++) {
-  //     sc[i].style.fontWeight = "normal";
-  //     sc[i].style.fontSize = "16px";
-  //   }
-  //   resp("恢复成功");
-  // }
+  }
 
   // 复制文本到剪贴板
   let chatRecords = "";
-  if (opt === 1 || opt === 2) {
+  if (opt.type === 1 || opt.type === 2) {
     // 获取main标签
     const mainNode = document.getElementsByTagName("main")[0];
     const chats = mainNode.children[0].children[0].children[0].children[0].children;
     for (const item of chats) {
       // 如果是发送内容
       if (item.className == "group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 dark:bg-gray-800") {
-        chatRecords += `\n【输入内容】\n${item.children[0].children[1].children[0].children[0].children[0].innerHTML}`;
+        chatRecords += opt.divider ? opt.divider : divider;
+        chatRecords += `\n${opt.inputPre ? opt.inputPre : inputPre}\n${item.children[0].children[1].children[0].children[0].children[0].innerHTML}`;
       } else if (item.className == "group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 bg-gray-50 dark:bg-[#444654]") {
-        chatRecords += `\n【输出内容】\n${item.children[0].children[1].children[0].children[0].children[0].innerText}\n-----`;
+        chatRecords += `\n${opt.inputSuf ? opt.inputSuf : inputSuf}\n${item.children[0].children[1].children[0].children[0].children[0].innerText}\n`;
+        chatRecords += opt.divider ? opt.divider : divider;
+        chatRecords += '\n\n'
       }
     }
-    if (opt === 1) {
+    if (opt.type === 1) {
       const a = document.createElement("a");
       a.style.display = "none";
       document.body.appendChild(a);
@@ -133,13 +116,14 @@ chrome.runtime.onMessage.addListener((req, sender, resp) => {
       })
       resp("复制成功，可直接在输入框粘贴");
     } else {
-      save2Text(chatRecords);
+      save2Text(chatRecords, opt);
       resp("导出成功");
     }
   }
 })
 
 
+// 复制文本至剪切板
 function toClipBoard(content) {
   const textarea = document.createElement('textarea');
   document.body.appendChild(textarea);
@@ -153,9 +137,11 @@ function toClipBoard(content) {
   }, 1000)
 }
 
-function save2Text(content) {
+// 导出文本
+function save2Text(content, opt) {
+  console.log(444, opt)
   const a = document.createElement("a");
-  a.setAttribute("download", "chatgpt导出内容");
+  a.setAttribute("download", opt.fileName ? opt.fileName : fileName);
   a.style.display = "none";
   const blob = new Blob([content]);
   a.setAttribute("href", URL.createObjectURL(blob));
--- js/popup.js
+++ js/popup.js
@@ -1,30 +1,60 @@
-tab_id = null;
+let tab_id = null;
+let curType = 0;
 
 // 判断当前tab的id
 chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
   tab_id = tabs[0].id;
 })
 
+const dealNode =  document.getElementsByClassName("deal-opt")[0];
+const copyNode =  document.getElementsByClassName("copy-opt")[0];
+const exportNode =  document.getElementsByClassName("export-opt")[0];
+const ansNode =  document.getElementById("ans");
+
 // 跳转到项目地址
 document.getElementById("proj").addEventListener("click", () => {
   window.open("https://github.com/quarkape/gpt-screen-shooter.git");
 })
 
-// 开始处理logo
-document.getElementById("deal").addEventListener("click", () => {
-  chrome.tabs.sendMessage(tab_id, {opt: 0}, (resp) => {
-      document.getElementById("ans").innerText = resp;
-  })
+const nodeArr = [dealNode, copyNode, exportNode];
+
+document.getElementsByClassName("opt-item")[0].addEventListener("click", (e) => {
+  const tagName = e.target.tagName.toLowerCase();
+  if (tagName === 'input' || tagName === "label") {
+    const forName = (tagName === "label" ? e.target.htmlFor : e.target.id) + "-opt";
+    console.log(forName)
+    nodeArr.forEach((item, index) => {
+      if (item.className.includes(forName)) {
+        item.classList.add("showconf");
+        curType = index;
+      } else {
+        item.classList.remove("showconf");
+      }
+    })
+  }
 })
 
-document.getElementById("copy").addEventListener("click", () => {
-  chrome.tabs.sendMessage(tab_id, {opt: 1}, (resp) => {
-      document.getElementById("ans").innerText = resp;
-  })
-})
-
-document.getElementById("export").addEventListener("click", () => {
-  chrome.tabs.sendMessage(tab_id, {opt: 2}, (resp) => {
-      document.getElementById("ans").innerText = resp;
+// 封装参数
+document.querySelector(".btn").addEventListener("click", () => {
+  const obj = {};
+  obj.type = curType;
+  if (curType === 0) {
+    obj.fontSize = document.getElementById("font-size").value;
+    obj.width = document.getElementById("width").value;
+    obj.inputColor = document.getElementById("input-color").value;
+    obj.outputColor = document.getElementById("output-color").value;
+    obj.showAvatar = document.getElementById("showa").checked;
+  } else if (curType === 1) {
+    obj.inputPre = document.getElementById("input-prea").value;
+    obj.inputSuf = document.getElementById("input-sufa").value;
+    obj.divider = document.getElementById("dividera").value;
+  } else {
+    obj.fileName = document.getElementById("export-name").value;
+    obj.inputPre = document.getElementById("input-pre").value;
+    obj.inputSuf = document.getElementById("input-suf").value;
+    obj.divider = document.getElementById("divider").value;
+  }
+  chrome.tabs.sendMessage(tab_id, obj, (resp) => {
+    ansNode.innerText = resp;
   })
 })
